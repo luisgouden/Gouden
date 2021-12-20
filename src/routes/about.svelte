b17f525dd5c8509a7405201926652eea0a705f08
luisgouden <porteiroperez+github@gmail.com>
2021-12-20 12:26:04 +0100

    que espanto

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -43,12 +43,8 @@ img {
 <article>
 <img src="about.png" alt="">
 <div class="two"> <strong>¡Hola!</strong>
-	<p>Me llamo Luís Porteiro y soy diseñador gráfico y frontend.</p>
-	<p>Como diseñador, me apasiona realizar composiciones utilizando principalmente degradados, formas geométricas y tipografías. Como front, me gusta exprimir al máximo todo el potencial que ofrecen el HTML y CSS, utilizando Javascript allí donde, de verdad, los otros dos no pueden llegar.</p>
-	<p>Al vivir con un pie en cada uno de estos dos mundos, me siento especialmente cómodo trabajando en procesos que suponen un puente entre diseño y desarrollo, así como en equipos multidisciplinares.</p>
-	<p>Y, si hasta aquí te ha parecido interesante lo que digo y quieres que te cuente más sobre mí...</p>
 	<p class="social"><strong>Mail: </strong><a href="mailto:luisgouden@gmail.com">luisgouden@gmail.com</a></p>
-	<p><strong>Instagram: </strong><a target="_blank" href="https://www.instagram.com/goudenjourney">Goudenjourney</a></p>
+	<p><strong>Instagram: </strong><a target="_blank" href="https://www.instagram.com/laitkraun">Luisgouden</a></p>
 	</div>
 </article>
 
