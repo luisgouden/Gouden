d03cb7a3f3a0b9e3c90928a7a1e62f8e6b5cca95
luisgouden <porteiroperez+github@gmail.com>
2023-02-25 16:29:49 +0100

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,51 +1,56 @@
 <script>
 </script>
+
 <style>
-
- article {
-  max-width: 1000px;
-  height: max-content;
-  line-height: 2;
-  font-size: 80%;
-  font-weight: 300;
-}
-img {
-  max-height: 400px;
-  max-width: 100%;
-}
-
-.two{
-  padding: 0 1rem;
-}
-
-.social {
-  margin-top: 1.5rem
-}
-
-@media (min-width:992px) {
-}
-@media (min-width:1200px) {
-  .two{
-    padding: 0;
+  article {
+    max-width: 1000px;
+    height: max-content;
+    line-height: 2;
+    font-size: 80%;
+    font-weight: 300;
+  }
+  img {
+    max-height: 400px;
+    max-width: 100%;
   }
 
-}
+  .two {
+    padding: 0 1rem;
+  }
 
+  .social {
+    margin-top: 1.5rem;
+  }
 
+  @media (min-width: 992px) {
+  }
+  @media (min-width: 1200px) {
+    .two {
+      padding: 0;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <section>
-
-<article>
-<img src="about.png" alt="">
-<div class="two"> <strong>¡Hi!</strong>
-	<p class="social"><strong>Mail: </strong><a href="mailto:luisgouden@gmail.com">luisgouden@gmail.com</a></p>
-	<p><strong>Instagram: </strong><a target="_blank" href="https://www.instagram.com/laitkraun">Luisgouden</a></p>
-	</div>
-</article>
-
+  <article>
+    <img src="about.png" alt="" />
+    <div class="two">
+      <strong>¡Hi!</strong>
+      <p class="social">
+        <strong>Mail: </strong><a href="mailto:luisgouden@gmail.com"
+          >luisgouden@gmail.com</a
+        >
+      </p>
+      <p>
+        <strong>Instagram: </strong><a
+          target="_blank"
+          href="https://www.instagram.com/laitkraun">Luisgouden</a
+        >
+      </p>
+    </div>
+  </article>
 </section>
