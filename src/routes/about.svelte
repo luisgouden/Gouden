a6049795541297a362090012e815b121d2160d1a
luisgouden <porteiroperez+github@gmail.com>
2023-04-20 21:29:49 +0200

    Revert "about.svelte"

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -330,8 +330,8 @@
 
 	let shuffledOptions = questions[currentQuestion].options.sort(() => Math.random() - 0.5);
 </script>
-
 <style>
+<<<<<<< HEAD
 	button:focus {
 		background-color: gold;
 	}
@@ -373,3 +373,53 @@
 		{/if}
 	{/if}
 
+=======
+
+ article {
+  max-width: 1000px;
+  height: max-content;
+  line-height: 2;
+  font-size: 80%;
+  font-weight: 300;
+}
+img {
+  max-height: 400px;
+  max-width: 100%;
+}
+
+.two{
+  padding: 0 1rem;
+}
+
+.social {
+  margin-top: 1.5rem
+}
+
+@media (min-width:992px) {
+}
+@media (min-width:1200px) {
+  .two{
+    padding: 0;
+  }
+
+}
+
+
+</style>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<section>
+
+<article>
+<img src="about.png" alt="">
+<div class="two"> <strong>¡Hi!</strong>
+	<p class="social"><strong>Mail: </strong><a href="mailto:luisgouden@gmail.com">luisgouden@gmail.com</a></p>
+	<p><strong>Instagram: </strong><a target="_blank" href="https://www.instagram.com/laitkraun">Luisgouden</a></p>
+	</div>
+</article>
+
+</section>
+>>>>>>> parent of d03cb7a (Update about.svelte)
